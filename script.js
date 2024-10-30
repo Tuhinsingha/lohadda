3dce420a9f6d075e125849f39962a572e326f8e7
Tuhinsingha <162964159+Tuhinsingha@users.noreply.github.com>
2024-10-31 01:19:35 +0530

    script.js

--- script.js
+++ script.js
@@ -36,25 +36,5 @@ function prevSlide() {
 // Initialize the first slide to be visible
 showSlide(currentIndex);
 
-// loadHeaderFooter.js
-function loadHeaderFooter() {
-    // Load header
-    fetch("header.html")
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById("header").innerHTML = data;
-      })
-      .catch(error => console.error("Error loading header:", error));
-  
-    // Load footer
-    fetch("footer.html")
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById("footer").innerHTML = data;
-      })
-      .catch(error => console.error("Error loading footer:", error));
-  }
-  
-  // Call the function on page load
-  document.addEventListener("DOMContentLoaded", loadHeaderFooter);
+
   
